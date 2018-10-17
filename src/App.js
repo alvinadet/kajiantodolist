c8d1e0fa32ab45c0690c8f1e989447d137c9418d
sodojiwo <alvinadetya@rocketmail.com>
2018-10-17 14:10:24 +0700

    kurang edit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,65 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
+  state = {
+    database: [],
+    data: ''
+  };
+  handleChange = e => {
+    this.setState({
+      data: e.target.value
+    });
+  };
+
+  getDatabase = () => {
+    const getTodo = JSON.parse(localStorage.getItem('todo'));
+    if (getTodo != null) {
+      return this.setState({
+        database: getTodo
+      });
+    } else {
+    }
+  };
+
+  addData = () => {
+    const database = this.state.database;
+    const data = this.state.data;
+    database.push(data);
+    localStorage.setItem('todo', JSON.stringify(database));
+
+    this.setState({
+      data: ''
+    });
+  };
+  deleteData = id => {
+    const database = this.state.database;
+    database.splice(id, 1);
+    localStorage.setItem('todo', JSON.stringify(database));
+    this.getDatabase();
+  };
+
+  componentDidMount() {
+    this.getDatabase();
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <input value={this.state.data} onChange={this.handleChange} />
+        <button onClick={() => this.addData()}>Tambah</button>
+        <ul>
+          {this.state.database.map((datum, id) => {
+            return (
+              <div>
+                <li key={id}>{datum}</li>
+                <button onClick={() => this.deleteData(id)}>Hapus</button>
+                <button onClick={() => this.getEdit(id)}>Edit</button>
+              </div>
+            );
+          })}
+        </ul>
       </div>
     );
   }
 }
-
 export default App;
